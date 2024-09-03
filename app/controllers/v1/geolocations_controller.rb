26348e88692ce9b2e073ac9ab8d37a140badeda8
gs <gs@deliverists.io>
2024-09-03 10:53:23 +0200

    Adding geolocation controller with tests

---
+++ app/controllers/v1/geolocations_controller.rb
@@ -0,0 +1,59 @@
+module V1
+  class GeolocationsController < ApplicationController
+    before_action :authenticate_token!, only: [ :create, :destroy ]
+    before_action :set_geolocation, only: [ :show, :destroy ]
+
+    def show
+      render json: @geolocation
+    end
+
+    def destroy
+      @geolocation.destroy
+      head :no_content
+    end
+
+    def create
+      geo_service = GeolocationService.new(geolocation_params[:ip_or_url])
+      geo_data = geo_service.fetch_data
+
+      if geo_data
+        @geolocation = Geolocation.new(
+          original_ip_or_url: geolocation_params[:ip_or_url],
+
+          ip: geo_data[:ip],
+          url: geo_data[:url],
+          country: geo_data[:country_name],
+          city: geo_data[:city],
+          latitude: geo_data[:latitude],
+          longitude: geo_data[:longitude]
+        )
+
+        if @geolocation.save
+          render json: @geolocation, status: :created
+        else
+          render json: @geolocation.errors, status: :unprocessable_entity
+        end
+      else
+        render json: { error: 'Unable to fetch geolocation data' }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def set_geolocation
+      @geolocation = Geolocation.find(params[:id])
+    end
+
+    def geolocation_params
+      params.require(:geolocation).permit(:ip_or_url, :url)
+    end
+
+    def authenticate_token!
+      render json: { error: "Unauthorized" }, status: :unauthorized unless valid_token?
+    end
+
+    def valid_token?
+      request.headers["Authorization"] == ENV["API_TOKEN"]
+    end
+  end
+end
---
+++ test/controllers/v1/geolocations_controller_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+require 'minitest/autorun'
+
+class V1::GeolocationsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @geolocation = geolocations(:google)
+    @headers = { 'Authorization' => ENV['API_TOKEN'] }
+  end
+
+  test "should get geolocation" do
+    get v1_geolocation_url(@geolocation)
+    assert_response :success
+  end
+
+  test "should return unprocessable_entity when GeolocationService returns nil" do
+    mock = Minitest::Mock.new
+    def mock.fetch_data; nil; end
+
+    GeolocationService.stub :new, mock do
+      post v1_geolocations_url, params: { geolocation: { ip_or_url: 'https://google.com' } }, headers: @headers
+    end
+    assert_nil assigns(:geo_data)
+    assert_response :unprocessable_entity
+  end
+
+  test "should create geolocation with valid token" do
+    post v1_geolocations_url, params: { geolocation: { ip_or_url: 'https://google.com' } }, headers: @headers
+    assert_response :created
+  end
+
+  test "should not create geolocation without valid token" do
+    post v1_geolocations_url, params: { geolocation: { ip_or_url: '134.201.250.155' } }
+    assert_response :unauthorized
+  end
+
+  test "should delete geolocation with valid token" do
+    delete v1_geolocation_url(@geolocation), headers: @headers
+    assert_response :no_content
+  end
+
+  test "should not delete geolocation without valid token" do
+    delete v1_geolocation_url(@geolocation)
+    assert_response :unauthorized
+  end
+end
